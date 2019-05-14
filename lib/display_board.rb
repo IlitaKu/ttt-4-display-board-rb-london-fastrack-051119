6eedb8b7f97046590b7f4d7ab603d27079a083b4
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-05-14 20:22:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,14 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board (board)
-  #puts "   |   |   "
-  #puts "-----------"
-  #puts "   |   |   "
-  #puts "-----------"
-  #puts "   |   |   "
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
 
-def display_board (board)
-board [" "," "," "," "," "," "," "," "," "]
-end
-puts display_board (board)
+puts dsipaly_board(board)
+#def display_board (board)
+#board [" "," "," "," "," "," "," "," "," "]
+#end
+#puts display_board (board)
